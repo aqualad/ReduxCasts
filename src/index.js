8b4dc0faf550336f11f48091af2ca9a1916010a7
Bryan Dodd <aqualad@gmail.com>
2016-08-07 03:40:23 -0400

    Adding basic SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 import secret_keys from '../secrets'
 
 const API_KEY = secret_keys.youtube_api;
@@ -8,7 +10,12 @@ const API_KEY = secret_keys.youtube_api;
 // Create a new component.  This component should produce
 // some HTML
 const App = () => {
-  return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+
 }
 
 // Take this component's generated HTML and put it
