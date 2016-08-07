84ac0ecb5776b322d4cefe5ad6330db5fb2be763
Bryan Dodd <aqualad@gmail.com>
2016-08-07 03:54:47 -0400

    Adding a simple onChange event

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
+class SearchBar extends Component {
+    render() {
+        return <input placeholder="Beanus" onChange={e => console.log(e.target.value)}/>
+    }
 };
 
 export default SearchBar;
