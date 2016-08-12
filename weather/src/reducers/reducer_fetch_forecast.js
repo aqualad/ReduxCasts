1d8714dd23521a70cb286d9745b0daa6e58eb58c
Bryan Dodd <aqualad@gmail.com>
2016-08-11 23:08:36 -0400

    Removing console.log statements

--- weather/src/reducers/reducer_fetch_forecast.js
+++ weather/src/reducers/reducer_fetch_forecast.js
@@ -1,7 +1,6 @@
 export default function(state = {}, action) {
     switch(action.type) {
         case 'FETCH_FORECAST':
-            console.log('FETCH_FORECAST REDUCER');
             return state;
     }
 
